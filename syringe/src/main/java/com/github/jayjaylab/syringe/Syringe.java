36cfb005f495c19b7861fbd296bfbd2ad3b088da
jjkim <jjkim@tmon.co.kr>
2016-05-29 12:43:29 +0900

    correct errors in pom.xml

---
+++ syringe/src/main/java/com/github/jayjaylab/syringe/Syringe.java
@@ -0,0 +1,10 @@
+package com.github.jayjaylab.syringe;
+
+/**
+ * Created by jjkim on 2016. 5. 28..
+ */
+public class Syringe {
+    public void a() {
+
+    }
+}
---
+++ syringe/src/test/java/com/github/jayjaylab/syringe/TestSyringe.java
@@ -0,0 +1,7 @@
+package com.github.jayjaylab.syringe;
+
+/**
+ * Created by jjkim on 2016. 5. 28..
+ */
+public class TestSyringe {
+}
